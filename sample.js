aeb3c44fe0b0fdbe02637d17db18e2d6b0a72752
QAmeluv <reshma.moncy42@gmail.com>
2022-11-30 18:21:25 +0400

    commiting  first commitin

---
+++ sample.js
@@ -0,0 +1,5 @@
+console.log("Hellow world!!");
+console.log("  I AM ATRYING SOMETHING");
+console.log("DONT KNOW IS THIS IS ");
+
+console.log("I am adding new chnages to tgis code to see how it works");
